717285dc821cc60a0525e51793b05d30ae17369a
Nisha Kumar-Mayernik <nisha.kumar-mayernik@intel.com>
2014-08-19 15:38:32 -0700

    Kernel cmdline to support fastboot GUI
    If i915.fastboot is not set, the GUI will not
    come up for fastboot. This is to prevent a regression
    from upcoming kernel changes
    
    Change-Id: I2b7d0c21463b74405b0a88e5b4a73b2640ce4c80
    Signed-off-by: Nisha Kumar-Mayernik <nisha.kumar-mayernik@intel.com>

--- base_x86_legacy/BoardConfig.mk
+++ base_x86_legacy/BoardConfig.mk
@@ -81,7 +81,8 @@ KERNEL_LOGLEVEL ?= 7
 
 BOARD_KERNEL_CMDLINE += \
         loglevel=$(KERNEL_LOGLEVEL) \
-        androidboot.hardware=$(TARGET_DEVICE)
+        androidboot.hardware=$(TARGET_DEVICE)\
+	i915.fastboot=1
 
 ##############################################################
 # Source: device/intel/mixins/groups/display-density/medium/BoardConfig.mk
