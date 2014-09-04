460ca3b334ef5b592e37f055d0e9ec702839a6ff
Jocelyn Falempe <jocelyn.falempe@intel.com>
2014-09-04 18:00:01 +0200

    [KF2] Add script to generate gpt.bin from gpt.ini
    Fastboot efi application can't parse a gpt.ini
    so this script convert the gpt.ini to a gpt.bin.
    
    Change-Id: Ibf3a98e0f6aa3c7c7d0776260dd13af440af4464
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>
    Reviewed-on: 245780
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>
    Tested-by: Boie, Andrew P <andrew.p.boie@intel.com>

---
+++ gpt_bin/gpt_ini2bin.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import uuid
+import struct
+import sys
+
+type_2_guid = {
+# official guid for gpt partition type
+    'fat' : 'ebd0a0a2-b9e5-4433-87c0-68b6b72699c7',
+    'esp' : 'c12a7328-f81f-11d2-ba4b-00a0c93ec93b',
+    'linux' : '0fc63daf-8483-4772-8e79-3d69d8477de4',
+    'linux-swap' : '0657fd6d-a4ab-43c4-84e5-0933c84b4f4f',
+# generated guid for android
+    'boot' : '49a4d17f-93a3-45c1-a0de-f50b2ebe2599',
+    'recovery' : '4177c722-9e92-4aab-8644-43502bfd5506',
+    'misc' : 'ef32a33b-a409-486c-9141-9ffb711f6266',
+    'metadata' : '20ac26be-20b7-11e3-84c5-6cfdb94711e9',
+    'tertiary' : '767941d0-2085-11e3-ad3b-6cfdb94711e9' }
+
+def zero_pad(s, size):
+    if (len(s) > size):
+        print 'error', len(s)
+    s += '\0' * (size - len(s))
+    return s
+
+def main():
+    if len(sys.argv) == 1:
+        print 'Usage : ', sys.argv[0], 'gpt_in1.ini gpt_in2.ini ...'
+        print '    write binary to stdout'
+        return
+
+    cfg = ConfigParser.SafeConfigParser()
+    for gpt_in in sys.argv[1:]:
+        cfg.read(gpt_in)
+    part = cfg.get('base', 'partitions').split()
+
+    magic = 0x6a8b0da1
+    start_lba = 0
+    if cfg.has_option('base', 'start_lba'):
+        start_lba = cfg.getint('base', 'start_lba')
+    npart = len(part)
+
+    out = sys.stdout
+    out.write(struct.pack('<I', magic))
+    out.write(struct.pack('<I', start_lba))
+    out.write(struct.pack('<I', npart))
+    for p in part:
+        length = cfg.get('partition.' + p, 'len')
+        out.write(struct.pack('<i', int(length)))
+
+        label = cfg.get('partition.' + p, 'label').encode('utf-16le')
+        out.write(zero_pad(label, 36 * 2))
+
+        guid_type = cfg.get('partition.' + p, 'type')
+        guid_type = uuid.UUID(type_2_guid[guid_type])
+        out.write(guid_type.bytes_le)
+
+        guid = uuid.UUID(cfg.get('partition.' + p, 'guid'))
+        out.write(guid.bytes_le)
+
+if __name__ == "__main__":
+    main()
