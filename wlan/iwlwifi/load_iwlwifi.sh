6dfba4bacdea3c5213ebd4b32ef4f7263e68524c
Emmanuel Berthier <emmanuel.berthier@intel.com>
2015-05-15 10:47:59 +0200

    [common] Add kernel modules location support
    Change-Id: Iaddb11f917d2df884eb8cc1bd0da1e013f8482b5
    Tracked-On: OAM-471
    Signed-off-by: Emmanuel Berthier <emmanuel.berthier@intel.com>
    Reviewed-on: 396381
    Reviewed-on: 400423

--- wlan/iwlwifi/load_iwlwifi.sh
+++ wlan/iwlwifi/load_iwlwifi.sh
@@ -1,14 +1,16 @@
-insmod system/lib/modules/compat.ko
+modules=`getprop ro.boot.moduleslocation`
+
+insmod $modules/compat.ko
 
 # Support building of stack-dev, that will build cfg80211.ko
 # and mac80211.ko instade of iwl-cfg80211.ko and  iwl-mac80211.ko.
-if [ -f /system/lib/modules/iwl-cfg80211.ko ]; then
-        insmod /system/lib/modules/iwl-cfg80211.ko
-        insmod /system/lib/modules/iwl-mac80211.ko
+if [ -f $modules/iwl-cfg80211.ko ]; then
+        insmod $modules/iwl-cfg80211.ko
+        insmod $modules/iwl-mac80211.ko
 else
-        insmod /system/lib/modules/cfg80211.ko
-        insmod /system/lib/modules/mac80211.ko
+        insmod $modules/cfg80211.ko
+        insmod $modules/mac80211.ko
 fi
 
-insmod /system/lib/modules/iwlwifi.ko nvm_file=iwl_nvm.bin d0i3_debug=1
-insmod /system/lib/modules/iwlmvm.ko always_on=1
+insmod $modules/iwlwifi.ko nvm_file=iwl_nvm.bin d0i3_debug=1
+insmod $modules/iwlmvm.ko always_on=1
