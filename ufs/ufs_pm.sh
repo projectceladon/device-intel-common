3097def64a7fa1dd948b78d0b9f184bd96717993
Dommelier, Henri <henri.dommelier@intel.com>
2015-07-16 17:15:18 +0100

    Revert "Add ufs SCSI LUNs runtime-pm config"
    This reverts commit 4aaa68269c6368c1c63c8515f36815b74db4ea33.
    
    Change-Id: Icab9d64629c2505c2307e06fc466cdb4ecf5844e
    Tracked-On: GMINL-13732
    Signed-off-by: Dommelier, Henri <henri.dommelier@intel.com>
    Reviewed-on: 391076

--- ufs/ufs_pm.sh
+++
@@ -1,7 +0,0 @@
-for lun in /sys/devices/pci0000\:00/0000\:00\:1d.0/host0/target0\:0\:0/0*; do
-    if [[ -d $lun/power ]]; then
-	echo 500  > $lun/power/autosuspend_delay_ms
-	echo auto > $lun/power/control
-    fi
-done
-
