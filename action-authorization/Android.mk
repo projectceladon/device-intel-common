978238abf38cb7dbb22d8191df40d2cf5280b207
Jeremy Compostella <jeremy.compostella@intel.com>
2015-07-16 16:12:01 +0200

    action-authorization: initial commit
    Provide a host command to build the action-authorization in response
    to the fastboot oem get-action-nonce.
    
    Usage: action-authorization OPTIONS
    
      Produce a PKCS7 answer message to a fastboot get-action-nonce.
      This PKCS7 can be sent using the fastboot flash action-authorization
      <file> command.
    
       --oak-cert, -O <file>          OAK certificate (PEM)
       --oak-private-key, -P <file>   private key file (PEM)
       --message, -M <string>         message received from get-action-nonce
                                      fastboot command
       --output-file, -F <file>       output file for the PKCS7 message
       --nopassword                   private key does not need a password
       --password <password>          private key password
       --verbose                      print all debug messages
       --supported-version, -V        print the nonce version supported
       --help, -h                     display this help and exit
    
    Change-Id: I121801fc875f7e88b8098e83ed0942c914810b5b
    Tracked-On: OAM-1277
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: 404379

---
+++ action-authorization/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := action-authorization
+LOCAL_SRC_FILES := action-authorization.c
+LOCAL_C_INCLUDES := external/openssl/include/
+LOCAL_CFLAGS := -Wall -Wextra -Werror
+LOCAL_SHARED_LIBRARIES := libcrypto-host
+
+include $(BUILD_HOST_EXECUTABLE)
