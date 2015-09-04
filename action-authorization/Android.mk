fed29e5c97657f41a507c9483f719329ea31c6cf
Jeremy Compostella <jeremy.compostella@intel.com>
2015-09-04 19:42:58 +0200

    action-authorization: link against openssl
    Change-Id: Icb8fc2811266ebe0d7c742ef817851620294dc99
    Tracked-On: OAM-1929
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: 408117

--- action-authorization/Android.mk
+++ action-authorization/Android.mk
@@ -6,6 +6,6 @@ LOCAL_MODULE := action-authorization
 LOCAL_SRC_FILES := action-authorization.c
 LOCAL_C_INCLUDES := external/openssl/include/
 LOCAL_CFLAGS := -Wall -Wextra -Werror
-LOCAL_SHARED_LIBRARIES := libcrypto-host
+LOCAL_SHARED_LIBRARIES := libcrypto-host2
 
 include $(BUILD_HOST_EXECUTABLE)
