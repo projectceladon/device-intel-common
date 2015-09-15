0f05e0933f546c5aa9a7f132a602fc3e43af4e91
Chokri ZAGROUBA <chokrix.zagrouba@intel.com>
2015-09-15 12:02:45 +0200

    Enable Configurable Audio Policy
    This patch enables Configurable Audio Policy for CHT
    
    Change-Id: I71a19d609e2651a00e16cc43e69fae0839411d43
    Tracked-On: OAM-1044
    Signed-off-by: Chokri ZAGROUBA <chokrix.zagrouba@intel.com>
    Reviewed-on: 411361

--- audio/parameter-framework/AndroidBoard.mk
+++ audio/parameter-framework/AndroidBoard.mk
@@ -610,3 +610,74 @@ REPLACE_PATTERNS := $(AUDIO_PATTERNS)
 include $(BUILD_REPLACE_PATTERNS_IN_FILE)
 
 ##################################################
+
+
+########## Policy PFW Structures #########
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := PolicyClass.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/parameter-framework/Structure/Policy
+LOCAL_SRC_FILES := Structure/Policy/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := PolicySubsystem.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_REQUIRED_MODULES := \
+    PolicySubsystem-CommonTypes.xml \
+    PolicySubsystem-Volume.xml \
+    libpolicy-subsystem
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/parameter-framework/Structure/Policy
+LOCAL_SRC_FILES := Structure/Policy/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := PolicySubsystem-CommonTypes.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/parameter-framework/Structure/Policy
+LOCAL_SRC_FILES := Structure/Policy/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := PolicySubsystem-Volume.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/parameter-framework/Structure/Policy
+LOCAL_SRC_FILES := Structure/Policy/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+######### Policy PFW Settings #########
+include $(CLEAR_VARS)
+LOCAL_MODULE := parameter-framework.policy
+LOCAL_MODULE_STEM := PolicyConfigurableDomains.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_RELATIVE_PATH := parameter-framework/Settings/Policy
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+        PolicyClass.xml \
+        PolicySubsystem.xml \
+        ParameterFrameworkConfigurationPolicy$(TUNING_SUFFIX).xml
+
+PFW_TOPLEVEL_FILE := $(TARGET_OUT_ETC)/parameter-framework/ParameterFrameworkConfigurationPolicy.xml
+PFW_CRITERIA_FILE := $(LOCAL_PATH)/policy_criteria.txt
+PFW_EDD_FILES := \
+        $(LOCAL_PATH)/Settings/Policy/device_for_strategy_media.pfw \
+        $(LOCAL_PATH)/Settings/Policy/device_for_strategy_phone.pfw \
+        $(LOCAL_PATH)/Settings/Policy/device_for_strategy_sonification.pfw \
+        $(LOCAL_PATH)/Settings/Policy/device_for_strategy_sonification_respectful.pfw \
+        $(LOCAL_PATH)/Settings/Policy/device_for_strategy_dtmf.pfw \
+        $(LOCAL_PATH)/Settings/Policy/device_for_strategy_enforced_audible.pfw \
+        $(LOCAL_PATH)/Settings/Policy/device_for_strategy_transmitted_through_speaker.pfw \
+        $(LOCAL_PATH)/Settings/Policy/device_for_strategy_accessibility.pfw \
+        $(LOCAL_PATH)/Settings/Policy/device_for_strategy_rerouting.pfw \
+        $(LOCAL_PATH)/Settings/Policy/strategy_for_stream.pfw \
+        $(LOCAL_PATH)/Settings/Policy/strategy_for_usage.pfw \
+        $(LOCAL_PATH)/Settings/Policy/device_for_input_source.pfw \
+        $(LOCAL_PATH)/Settings/Policy/volumes.pfw
+
+include $(BUILD_PFW_SETTINGS)
+
