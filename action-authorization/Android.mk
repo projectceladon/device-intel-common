39714befcac287f4939ebae1b9da569b5d18b401
Jeremy Compostella <jeremy.compostella@intel.com>
2015-09-28 13:32:49 +0200

    action-authorization: link statically to openssl/c++ libraries
    Change-Id: Iaccc48189f341e85512e38b62d23c6efb3024afd
    Tracked-On: OAM-4013
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: 416711

--- action-authorization/Android.mk
+++ action-authorization/Android.mk
@@ -3,9 +3,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := action-authorization
+LOCAL_LDLIBS += -ldl
 LOCAL_SRC_FILES := action-authorization.c
 LOCAL_C_INCLUDES := external/openssl/include/
 LOCAL_CFLAGS := -Wall -Wextra -Werror
-LOCAL_SHARED_LIBRARIES := libcrypto-host2
+LOCAL_CXX_STL = libc++_static
+LOCAL_STATIC_LIBRARIES := libcrypto_static2
 
 include $(BUILD_HOST_EXECUTABLE)
