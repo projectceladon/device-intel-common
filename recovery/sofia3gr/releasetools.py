89a90a1919a8cdf09a077fb625de42dd38a7f170
adattatr <anisha.dattatraya.kulkarni@intel.com>
2015-10-05 14:02:51 -0700

    Enable OTAs on COHOS3GR.
    Incremental and Full OTAs functionality for variants is
    added in ota_deployment_fixup. Functions like get_file_data
    and AddBinaryFiles are used by ota_deployment_fixup and so
    are moved to intel_common.py for reusability.
    
    Change-Id: I1aa5aa8d8dc850840b8120402e7924c797ed4ae6
    Tracked-On: OAM-13125
    Signed-off-by: adattatr <anisha.dattatraya.kulkarni@intel.com>
    Reviewed-on: 450599

--- recovery/sofia3gr/releasetools.py
+++ recovery/sofia3gr/releasetools.py
@@ -2,90 +2,66 @@
 # Copyright (C) 2015 Intel Corporation
 #
 # Releasetools hook for Sofia
-#
-# Here you can add variant specific custom steps to the OTA process
-#
+# This file contains releasetools extensions for updating the bootloader
+# related partitions for Sofia3GR devices.
+
 import common
 import os
 import zipfile
-from cStringIO import StringIO
+import sys
+
+sys.path.append("device/intel/build/releasetools")
+import intel_common
 
 OPTIONS = common.OPTIONS
 patchinfo = None
-
-
-def get_file_data(tmpdir, path):
-    provdata_name = os.path.join(tmpdir, "RADIO", "provdata.zip")
-    with zipfile.ZipFile(provdata_name) as provdata_zip:
-        data = provdata_zip.read(path)
-    return common.File(path, data)
-
+verbatim = None
 
 def trigger_fwupdate(info):
   _, misc_device = common.GetTypeAndDevice("/misc", OPTIONS.info_dict)
   info.script.script.append('intel_install_firmware_update("%s");'
       % misc_device)
 
-
-def AddBinaryFiles(info):
-  provdata_name = os.path.join("RADIO", "provdata.zip")
-  if provdata_name in info.input_zip.namelist():
-    with zipfile.ZipFile(StringIO(info.input_zip.read(provdata_name))) as provdata_zip:
-        fwu_image = provdata_zip.read("fwu_image.bin")
-    common.ZipWriteStr(info.output_zip, "fwu_image.bin", fwu_image)
-  else:
-    # fwu_image will get populated by ota_deployment_fixup script
-    pass
-
-  info.script.SetProgress(0.6) #Set progress bar to end of FLS extraction
-
-  info.script.WriteRawImage("/fwupdate", "fwu_image.bin");
-  trigger_fwupdate(info)
-
-
-#These steps are called in the end of an OTA update
+# These steps are called in the end of an OTA update
 def FullOTA_InstallEnd(info):
   print "Intel: vendor specific OTA end hook for full OTA update"
-  AddBinaryFiles(info)
+  intel_common.AddFWImageFile(info.input_zip, info.output_zip)
 
+  # Set progress bar to end of FLS extraction
+  info.script.SetProgress(0.6)
+  info.script.WriteRawImage("/fwupdate", "fwu_image.bin");
+  trigger_fwupdate(info)
 
 # Called during an incremental update before any changes are made
 def IncrementalOTA_VerifyEnd(info):
   global patchinfo
-
+  global verbatim
   print "Calculating fwupdate patch information"
-  src_fwupdate = get_file_data(OPTIONS.source_tmp, "fwu_image.bin")
-  tgt_fwupdate = get_file_data(OPTIONS.target_tmp, "fwu_image.bin")
 
-  diffs = [common.Difference(tgt_fwupdate, src_fwupdate)]
-  common.ComputeDifferences(diffs)
-
-  tf, sf, d = diffs[0].GetPatch()
-  # If the patch size is almost as big as the actual file don't bother
-  if d is None or len(d) > tf.size * 0.95:
-    print "Firmware update image will be included verbatim"
-    return
-
-  common.ZipWriteStr(info.output_zip, "patch/fwu_image.bin.p", d)
   fwu_type, fwu_device = common.GetTypeAndDevice("/fwupdate", OPTIONS.info_dict)
-  # This check ensure fwupdate partition is in an expected state before
-  # the OTA system makes any changes
-  info.script.PatchCheck("%s:%s:%d:%s:%d:%s" %
-                         (fwu_type, fwu_device, sf.size,
-                          sf.sha1, tf.size, tf.sha1))
-  patchinfo = (fwu_type, fwu_device, sf, tf)
-
+  verbatim, fwu_patch, output_files = intel_common.ComputeFWUpdatePatches(OPTIONS.source_tmp, OPTIONS.target_tmp)
+  if not verbatim and fwu_patch:
+    (tf,sf) = fwu_patch
+    # The below check ensures fwupdate partition is in an expected state before
+    # the OTA system makes any changes
+    info.script.PatchCheck("%s:%s:%d:%s:%d:%s" %
+                           (fwu_type, fwu_device, sf.size,
+                            sf.sha1, tf.size, tf.sha1))
+    patchinfo = (fwu_type, fwu_device, sf, tf)
+    common.ZipWriteStr(info.output_zip, "patch/fwu_image.bin.p", output_files)
 
 def IncrementalOTA_InstallEnd(info):
   print "Intel: vendor specific OTA end hook for incremental OTA update"
-  if not patchinfo:
-    AddBinaryFiles(info)
-    return
+  if verbatim:
+    intel_common.AddFWImageFile(info.target_zip, info.output_zip)
 
-  fwu_type, fwu_device, sf, tf = patchinfo
-  info.script.ApplyPatch("%s:%s:%d:%s:%d:%s" % (fwu_type, fwu_device, sf.size,
-                                                sf.sha1, tf.size, tf.sha1),
-                         "-", tf.size, tf.sha1, sf.sha1,
-                         "patch/fwu_image.bin.p")
+    # Set progress bar to end of FLS extraction
+    info.script.SetProgress(0.6)
+    info.script.WriteRawImage("/fwupdate", "fwu_image.bin");
+  elif patchinfo:
+    fwu_type, fwu_device, sf, tf = patchinfo
+    info.script.ApplyPatch("%s:%s:%d:%s:%d:%s" % (fwu_type, fwu_device, sf.size,
+                                                  sf.sha1, tf.size, tf.sha1),
+                           "-", tf.size, tf.sha1, sf.sha1,
+                           "patch/fwu_image.bin.p")
   trigger_fwupdate(info)
-
