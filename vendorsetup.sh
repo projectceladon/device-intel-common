47d952d75d4f1afc2ab8aa127db7d0968221272e
Jocelyn Falempe <jocelyn.falempe@intel.com>
2015-11-23 18:31:59 +0100

    add mkernel and mbimg
    Change-Id: Id598d5762615478200dc2872661c6a78c1b5f617
    Tracked-On: OAM-10180
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>
    Reviewed-on: 439541

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,3 +15,25 @@ function binarydownloader(){
 }
 binarydownloader --check-creds
 fi
+
+mkernel() {
+    local T=$(gettop)
+    local KERNEL_MAKEFILE=$(\cd "$T" && find device -name ${TARGET_PRODUCT}.mk | sed -e "s/${TARGET_PRODUCT}\.mk/AndroidBoard\.mk/")
+    local ARGS=$(echo "$*"; [ -z "$(echo $* | sed -e "s/-[^ ]*//g" -e "s/ //g")" ] && echo "kernel")
+    local POKY_MAKEFILE=prebuilts/gcc/linux-x86/x86/x86_64-linux-poky/Android.mk
+
+    if [ ! -f $T/$KERNEL_MAKEFILE ] ; then
+        echo "Kernel makefile not found for TARGET_PRODUCT=$TARGET_PRODUCT. abort" 1>&2
+        return 1
+    fi
+    ONE_SHOT_MAKEFILE="$KERNEL_MAKEFILE $POKY_MAKEFILE" make -C $T -f build/core/main.mk $ARGS
+}
+
+mbimg() {
+    local T=$(gettop)
+
+    mkernel kernel $* || return
+
+    echo ===[ Generating Ramdisk and ${ANDROID_PRODUCT_OUT}/boot.img]===
+    (\cd "$T" && make ramdisk-nodeps bootimage-nodeps $* )
+}
