3a5020703e9748008c76af9e3eaf4d1155a451d3
Zhang Gary <gary.zhang@intel.com>
2015-11-30 13:34:35 +0800

    Add feature for parallel scan
    To parallel package scan
    
    Change-Id: I9a2ce02edf085b3d4308050a7209c077de9e3078
    Signed-off-by: Zhang Gary <gary.zhang@intel.com>
    Tracked-On: OAM-10391
    Reviewed-on: 442090

--- feature_config/Android.mk
+++ feature_config/Android.mk
@@ -1,5 +1,6 @@
 # Add features to the list as new features are supported in the build tree
 INTEL_SUPPORTED_FEATURES := \
+        INTEL_FEATURE_PARALLEL_SCAN \
         INTEL_FEATURE_AWARESERVICE \
         INTEL_FEATURE_ADAPTIVE_AUTHENTICATION \
         INTEL_FEATURE_CTA_SUPPORT \
