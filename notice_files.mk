8d7aa3715aa6cff7e13249aa199e509b093275f1
Benjamin Pujol <benjamin.pujol@intel.com>
2016-01-18 03:38:15 +0100

    Run check for vendor partition only on makefiles used to build the target
    In order to check if the rules for the vendor partition are respected
    a rule in the makefiles will help to warn if a module
    is not installed in vendor when it should be.
    
    Change-Id: Icea57cc6b755a3354e52b59d7a48588fbf44a7e0
    Tracked-On: OAM-16968
    Signed-off-by: Benjamin Pujol <benjamin.pujol@intel.com>
    Reviewed-on: 459466

---
+++ notice_files.mk
@@ -0,0 +1,15 @@
+# If Makefile is located in vendor/intel directory
+# but module is not installed in /vendor partition or /system/vendor
+# it is a delinquant code.
+# Add it to DELINQUANT_VENDOR_MODULES list to display
+# a warning later during build process
+ifneq (,$(filter vendor/intel%,$(LOCAL_MODULE_MAKEFILE)))
+ifneq (,$(filter $(PRODUCT_OUT)%,$(LOCAL_INSTALLED_MODULE)))
+ifeq (,$(filter $(PRODUCT_OUT)/system/vendor/% $(PRODUCT_OUT)/vendor/%,$(LOCAL_INSTALLED_MODULE)))
+DELINQUANT_VENDOR_MODULES := $(DELINQUANT_VENDOR_MODULES) $(LOCAL_MODULE):$(LOCAL_INSTALLED_MODULE):$(LOCAL_MODULE_MAKEFILE)
+endif
+endif
+endif
+
+# Call original makefile from Android build system
+include $(BUILD_SYSTEM)/notice_files.mk
