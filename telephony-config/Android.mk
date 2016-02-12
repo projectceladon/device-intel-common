4394a7b726bd068afd3b11bfc218e2c7cf683a3d
Cesar DE OLIVEIRA <cesar.de.oliveira@intel.com>
2016-02-12 15:57:54 +0100

    telephony: scalability
    TLV files used to apply modem customization and XML files used to
    configure telephony framework are moved to this new repo.
    
    Change-Id: I651d25a6343bdc428e5e98e65c1c1964f288a83d
    Tracked-On: OAM-19945
    Signed-off-by: Cesar DE OLIVEIRA <cesar.de.oliveira@intel.com>
    Reviewed-on: 469828

---
+++ telephony-config/Android.mk
@@ -0,0 +1,61 @@
+LOCAL_PATH := $(call my-dir)
+
+define copy_file
+include $(CLEAR_VARS)
+LOCAL_PATH := $(2)
+LOCAL_MODULE := $(notdir $(1))
+LOCAL_SRC_FILES := $(3)/$(strip $1)
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $4
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+endef
+
+# Creation of hash file
+include $(CLEAR_VARS)
+LOCAL_MODULE := hash
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/firmware/telephony
+include $(BUILD_SYSTEM)/base_rules.mk
+$(LOCAL_BUILT_MODULE) : $(STREAMLINE_TLVS $(MDM_FW_FILES))
+	@echo "Building telephony hash file"
+	$(hide) rm -fr $(dir $@)
+	$(hide) mkdir -p $(dir $@)
+	$(hide) cat $(STREAMLINE_TLVS) $(MDM_FW_FILES) | md5sum | tr -d ' -' > $@
+
+# TCS1. @TODO: remove this
+TCS_XMLS := $(notdir $(wildcard $(LOCAL_PATH)/hw_config/tcs/*.xml))
+$(foreach xml, $(TCS_XMLS), $(eval $(call copy_file, $(xml), $(LOCAL_PATH), hw_config/tcs, $(TARGET_OUT_VENDOR)/etc/telephony)))
+
+# TCS2. @TODO: add TCS2 xml files
+STREAMLINE_XMLS := $(notdir $(wildcard $(LOCAL_PATH)/streamline/xmls/*.xml))
+$(foreach xml, $(STREAMLINE_XMLS), $(eval $(call copy_file, $(xml), $(LOCAL_PATH), streamline/xmls, $(TARGET_OUT_VENDOR)/etc/telephony/tcs/streamline)))
+
+# TLV files
+STREAMLINE_TLVS := $(notdir $(wildcard $(LOCAL_PATH)/streamline/tlvs/*.tlv))
+$(foreach tlv, $(STREAMLINE_TLVS), $(eval $(call copy_file, $(tlv), $(LOCAL_PATH), streamline/tlvs, $(TARGET_OUT_VENDOR)/firmware/telephony)))
+
+# modem firmwares
+MDM_FW_FILES := $(foreach mdm, $(BOARD_MODEM_LIST), $(wildcard vendor/intel/fw/modem/IMC/*/$(mdm)/*.fls))
+$(foreach fw, $(MDM_FW_FILES), $(eval $(call copy_file, $(fw), ., , $(TARGET_OUT_VENDOR)/firmware/telephony)))
+
+# Geo file used by M2 module
+XML_GEO_FILE := $(foreach mdm, $(BOARD_MODEM_LIST), $(wildcard vendor/intel/fw/modem/IMC/*/$(mdm)/*.xml))
+$(if $(strip $(XML_GEO_FILE)), $(eval $(call copy_file, $(XML_GEO_FILE), ., , $(TARGET_OUT_VENDOR)/firmware/telephony)))
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mdm_fw_pkg
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := intel
+LOCAL_REQUIRED_MODULES := $(STREAMLINE_XMLS) $(STREAMLINE_TLVS) $(XML_GEO_FILE) $(notdir $(MDM_FW_FILES)) hash
+include $(BUILD_PHONY_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := tcs_hw_xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := intel
+LOCAL_REQUIRED_MODULES := $(TCS_XMLS)
+include $(BUILD_PHONY_PACKAGE)
