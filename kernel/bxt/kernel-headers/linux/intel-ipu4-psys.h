dc7b674c59651c916276ebf45b00671a75acfbc7
Kriti Pachhandara <kriti.pachhandara@intel.com>
2016-09-09 14:52:51 +0300

    Update psys kernel header file
    Tracked-On:#JICSF-612
    
    Change-Id: I9e1e2b1ae8cd09e77fa0f20f0b3ef7318d85dea9
    Signed-off-by: Kriti Pachhandara <kriti.pachhandara@intel.com>
    Reviewed-on: 538993
    Reviewed-by: Hogander, Jouni <jouni.hogander@intel.com>
    Reviewed-by: Koski, Anttu <anttu.koski@intel.com>
    Tested-by: Tao, Wei <wei.tao@intel.com>
    Reviewed-by: Onkalo, Samu <samu.onkalo@intel.com>
    Reviewed-by: Han, Bin B <bin.b.han@intel.com>
    Reviewed-by: Ding, Yunliang <yunliang.ding@intel.com>
    Reviewed-by: Meng, WeiX <weix.meng@intel.com>
    Tested-by: Meng, WeiX <weix.meng@intel.com>

--- kernel/bxt/kernel-headers/linux/intel-ipu4-psys.h
+++ kernel/bxt/kernel-headers/linux/intel-ipu4-psys.h
@@ -46,59 +46,60 @@ struct intel_ipu4_psys_dma_buf {
   uint32_t data_offset;
   uint32_t bytes_used;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-  uint32_t reserved[2];
+  uint32_t flags;
+  uint32_t reserved;
 } __attribute__((packed));
 struct intel_ipu4_psys_buffer {
-  uint64_t len;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+  uint64_t len;
   void __user * userptr;
   int fd;
   uint32_t flags;
-  uint32_t reserved[2];
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+  uint32_t reserved[2];
 } __attribute__((packed));
 #define INTEL_IPU4_BUFFER_FLAG_INPUT (1 << 0)
 #define INTEL_IPU4_BUFFER_FLAG_OUTPUT (1 << 1)
-#define INTEL_IPU4_BUFFER_FLAG_MAPPED (1 << 2)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define INTEL_IPU4_BUFFER_FLAG_MAPPED (1 << 2)
 #define INTEL_IPU4_BUFFER_FLAG_NO_FLUSH (1 << 3)
 #define INTEL_IPU4_PSYS_CMD_PRIORITY_HIGH 0
 #define INTEL_IPU4_PSYS_CMD_PRIORITY_MED 1
-#define INTEL_IPU4_PSYS_CMD_PRIORITY_LOW 2
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define INTEL_IPU4_PSYS_CMD_PRIORITY_LOW 2
 #define INTEL_IPU4_PSYS_CMD_PRIORITY_NUM 3
 struct intel_ipu4_psys_command {
   uint64_t issue_id;
-  uint32_t id;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+  uint32_t id;
   uint32_t priority;
   void __user * pg_manifest;
   struct intel_ipu4_psys_dma_buf __user * buffers;
-  int pg;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+  int pg;
   uint32_t pg_manifest_size;
   uint32_t bufcount;
   uint32_t min_psys_freq;
-  uint32_t reserved[2];
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+  uint32_t reserved[2];
 } __attribute__((packed));
 struct intel_ipu4_psys_manifest {
   uint32_t index;
-  uint32_t size;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+  uint32_t size;
   void __user * manifest;
   uint32_t reserved[5];
 } __attribute__((packed));
-#define INTEL_IPU4_IOC_QUERYCAP _IOR('A', 1, struct intel_ipu4_psys_capability)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define INTEL_IPU4_IOC_QUERYCAP _IOR('A', 1, struct intel_ipu4_psys_capability)
 #define INTEL_IPU4_IOC_MAPBUF _IOWR('A', 2, int)
 #define INTEL_IPU4_IOC_UNMAPBUF _IOWR('A', 3, int)
 #define INTEL_IPU4_IOC_GETBUF _IOWR('A', 4, struct intel_ipu4_psys_buffer)
-#define INTEL_IPU4_IOC_PUTBUF _IOWR('A', 5, struct intel_ipu4_psys_buffer)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define INTEL_IPU4_IOC_PUTBUF _IOWR('A', 5, struct intel_ipu4_psys_buffer)
 #define INTEL_IPU4_IOC_QCMD _IOWR('A', 6, struct intel_ipu4_psys_command)
 #define INTEL_IPU4_IOC_DQEVENT _IOWR('A', 7, struct intel_ipu4_psys_event)
 #define INTEL_IPU4_IOC_CMD_CANCEL _IOWR('A', 8, struct intel_ipu4_psys_command)
-#define INTEL_IPU4_IOC_GET_MANIFEST _IOWR('A', 9, struct intel_ipu4_psys_manifest)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define INTEL_IPU4_IOC_GET_MANIFEST _IOWR('A', 9, struct intel_ipu4_psys_manifest)
 #endif
