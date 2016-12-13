bb1bb39fbe0b300fc00fe985af4ca562686c6761
biyilix <biyix.li@intel.com>
2016-12-13 16:49:12 +0800

    Porting. Fix [N.car]Incremental OTA issue.
    Porting refer:
    547508
    544702
    #/c/556251
    
    Change-Id: Id41f8645a416a17b865c790f360f8cbdddd2a68c
    Tracked-On: OAM-38621
    Signed-off-by: biyilix <biyix.li@intel.com>
    Reviewed-on: 557316

--- recovery/bootloader/releasetools.py
+++ recovery/bootloader/releasetools.py
@@ -1,11 +1,206 @@
 import common
+import fnmatch
+import os
+import sys
+import tempfile
+import subprocess
+import zipfile
+import json
+import hashlib
+import os, errno
+import shlex
+import shutil
+import imp
+import time
+import collections
+import time
+import re
+import random
+
+from cStringIO import StringIO
+
+sys.path.append("device/intel/build/releasetools")
+import intel_common
+
+_SIMG2IMG = "out/host/linux-x86/bin/simg2img"
+_FASTBOOT = "out/host/linux-x86/bin/fastboot"
+_VERIFYTOOL = "device/intel/common/recovery/verify_from_ota"
+OPTIONS = common.OPTIONS
+verify_multiboot = None
+multiboot_patchinfo = None
+
+def hash_sparse_ext4_image(image_name):
+
+  print "TFP: ", image_name
+  t = tempfile.NamedTemporaryFile(delete=False)
+  OPTIONS.tempfiles.append(t.name)
+  t.close()
+
+  subprocess.check_call([_SIMG2IMG, image_name, t.name])
+  remain = os.path.getsize(t.name)
+  fd = open(t.name)
+  hc = hashlib.sha1()
+
+  while (remain):
+      data = fd.read(1024 * 1024)
+      hc.update(data)
+      remain = remain - len(data)
+
+  print "system hash", hc.hexdigest()
+  fd.close()
+  return hc.hexdigest()
+
+
+def GetBootloaderImagesfromFls(unpack_dir, variant=None):
+  """ ABL bootloaders, comprise of
+  various partitions. The partitions are fixed """
+  global verify_multiboot
+
+  additional_data_hash = collections.OrderedDict()
+  #need get boot/recovery/system data first
+  curr_loader = "bootloader.img"
+  loader_filepath = os.path.join(unpack_dir, "RADIO", curr_loader)
+  if not os.path.exists(loader_filepath):
+      print "Can't find ", loader_filepath
+      print "GetBootloaderImagesfromFls failed"
+      return
+  loader_file = open(loader_filepath)
+  loader_data = loader_file.read()
+  additional_data_hash['bootloader'] = loader_data
+  loader_file.close()
+
+  #need get boot/recovery/system data first
+  curr_loader = "boot.img"
+  loader_filepath = os.path.join(unpack_dir, "IMAGES", curr_loader)
+  if not os.path.exists(loader_filepath):
+      print "Can't find ", loader_filepath
+      print "GetBootloaderImagesfromFls failed"
+      return
+  loader_file = open(loader_filepath)
+  loader_data = loader_file.read()
+  additional_data_hash['boot'] = loader_data
+  loader_file.close()
+
+  curr_loader = "recovery.img"
+  loader_filepath = os.path.join(unpack_dir, "IMAGES", curr_loader)
+  if not os.path.exists(loader_filepath):
+      print "Can't find ", loader_filepath
+      print "GetBootloaderImagesfromFls failed"
+      return
+  loader_file = open(loader_filepath)
+  loader_data = loader_file.read()
+  additional_data_hash['recovery'] = loader_data
+  loader_file.close()
+
+  curr_loader = "system.img"
+  loader_filepath = os.path.join(unpack_dir, "IMAGES", curr_loader)
+  if not os.path.exists(loader_filepath):
+      print "Can't find ", loader_filepath
+      print "GetBootloaderImagesfromFls failed"
+      return
+  additional_data_hash['system'] = str(hash_sparse_ext4_image(loader_filepath))
+
+  if verify_multiboot is not None:
+    curr_loader = "multiboot.img"
+    loader_filepath = os.path.join(unpack_dir, "RADIO", curr_loader)
+    if os.path.exists(loader_filepath):
+        loader_file = open(loader_filepath)
+        loader_data = loader_file.read()
+        additional_data_hash['multiboot'] = loader_data
+        loader_file.close()
+
+  curr_loader = "vendor.img"
+  loader_filepath = os.path.join(unpack_dir, "IMAGES", curr_loader)
+  if not os.path.exists(loader_filepath):
+      print "Can't find ", loader_filepath
+      print "GetBootloaderImagesfromFls failed"
+      return
+  additional_data_hash['vendor'] = str(hash_sparse_ext4_image(loader_filepath))
+
+  return additional_data_hash
+
+def Get_verifydata(info,infoinput):
+  print "Trying to get verify data..."
+  variant = None
+
+  for app in [_SIMG2IMG, _FASTBOOT, _VERIFYTOOL]:
+      if not os.path.exists(app):
+          print "Can't find", app
+          print "Get_verifydata failed"
+          return
+
+  print "Extracting bootloader archive..."
+  additional_data = GetBootloaderImagesfromFls(infoinput,None)
+  if not additional_data:
+    print "additional_data is None"
+    return
+  bootloader_sizes = ""
+  imghash_value = "..."
+  imghash_bootloader = ""
+
+  for imgname, imgdata in additional_data.iteritems():
+      if imgname != 'bootloader' and imgname != 'system' and imgname != 'boot' and imgname != 'recovery' \
+          and imgname != 'vendor' and imgname != 'multiboot':
+          bootloader_sizes += ":" + str(len(imgdata))
+      if imgname != 'system' and imgname != 'vendor':
+          imghash_value += "\n(bootloader) target: /" + str(imgname)
+          imghash_value += "\n(bootloader) hash: " + str(hashlib.sha1(imgdata).hexdigest())
+      if imgname == 'system' or imgname == 'vendor':
+          imghash_value += "\n(bootloader) target: /" + str(imgname)
+          imghash_value += "\n(bootloader) hash: " + str(imgdata)
+  imghash_value += imghash_bootloader + "\n"
+
+  common.ZipWriteStr(info.output_zip, "verify/fastbootcmd.txt", bootloader_sizes)
+  common.ZipWriteStr(info.output_zip, "verify/hashesfromtgt.txt", imghash_value)
+  #write fastboot tool
+  f = open(_FASTBOOT, "rb")
+  fbbin = f.read()
+  common.ZipWriteStr(info.output_zip, "verify/fastboot", fbbin, perms=0o755)
+  f.close()
+  #Save verify_from_ota script to OTA package
+  f = open(_VERIFYTOOL, "r")
+  data = f.read()
+  common.ZipWriteStr(info.output_zip, "verify/verify_from_ota", data, perms=0o755)
+  f.close()
+
+def WriteMultiboot(info, multiboot_img):
+  common.ZipWriteStr(info.output_zip, "multiboot.img", multiboot_img)
+  try:
+    info.script.WriteRawImage("/multiboot", "multiboot.img")
+  except (IOError, KeyError):
+    print "no /multiboot partition in target_files; skipping install"
 
 def WriteBldr(info, bootloader_img):
   common.ZipWriteStr(info.output_zip, "bootloader.img", bootloader_img)
   info.script.WriteRawImage("/bootloader", "bootloader.img")
   info.script.script.append('capsule_abl("m1:ifwi_update.bin");')
 
+def IncrementalOTA_VerifyEnd(info):
+  global multiboot_patchinfo
+
+  fstab = info.script.info.get("fstab", None)
+  try:
+    if fstab['/multiboot'].device:
+      multibootimg_type, multibootimg_device = common.GetTypeAndDevice("/multiboot", OPTIONS.info_dict)
+      verbatim_targets, m_patch_list, output_files = \
+              intel_common.ComputeBinOrImgPatches(OPTIONS.source_tmp,
+                                                  OPTIONS.target_tmp, "multiboot.img")
+      if output_files is None:
+        print "multiboot.img is none, skipping install"
+      else:
+        print "multiboot.img is exist. Add the patch."
+        if not verbatim_targets and m_patch_list:
+          (tf,sf) = m_patch_list
+          info.script.PatchCheck("%s:%s:%d:%s:%d:%s" %
+                                 (multibootimg_type, multibootimg_device, sf.size,
+                                  sf.sha1, tf.size, tf.sha1))
+          multiboot_patchinfo = (multibootimg_type, multibootimg_device, sf, tf)
+          common.ZipWriteStr(info.output_zip, "patch/multiboot.img.p", output_files)
+  except (IOError, KeyError):
+    print "No multiboot partition in iOTA Verify"
+
 def FullOTA_InstallEnd(info):
+  global verify_multiboot
   try:
     bootloader_img = info.input_zip.read("RADIO/bootloader.img")
   except KeyError:
@@ -14,7 +209,24 @@ def FullOTA_InstallEnd(info):
     print "Add update for bootloader"
     WriteBldr(info, bootloader_img)
 
+    try:
+      multiboot_img = info.input_zip.read("RADIO/multiboot.img")
+    except (IOError, KeyError):
+      print "no multiboot.img in target target_files; skipping install"
+    else:
+      fstab = info.script.info.get("fstab", None)
+      try:
+        if fstab['/multiboot'].device:
+          print "multiboot partition exist and multiboot.img changed; adding it"
+          WriteMultiboot(info, multiboot_img)
+          verify_multiboot = 1
+      except (IOError, KeyError):
+        print "No multiboot partition"
+
+    Get_verifydata(info, info.input_tmp)
+
 def IncrementalOTA_InstallEnd(info):
+  global verify_multiboot
   try:
     target_bootloader_img = info.target_zip.read("RADIO/bootloader.img")
     try:
@@ -27,6 +239,26 @@ def IncrementalOTA_InstallEnd(info):
     else:
       print "bootloader.img changed; adding it"
       WriteBldr(info, target_bootloader_img)
+
+    try:
+      multiboot_img = info.target_zip.read("RADIO/multiboot.img")
+    except (IOError, KeyError):
+      print "no multiboot.img in target target_files; skipping install"
+    else:
+      fstab = info.script.info.get("fstab", None)
+      try:
+        if fstab['/multiboot'].device:
+          print "multiboot partition exist and multiboot.img changed; adding it"
+          multibootimg_type, multibootimg_device, sf, tf = multiboot_patchinfo
+          info.script.ApplyPatch("%s:%s:%d:%s:%d:%s" % (multibootimg_type, multibootimg_device, sf.size,
+                                                        sf.sha1, tf.size, tf.sha1),
+                                 "-", tf.size, tf.sha1, sf.sha1,
+                                 "patch/multiboot.img.p")
+          verify_multiboot = 1
+      except (IOError, KeyError):
+        print "No multiboot partition"
+
+      Get_verifydata(info, OPTIONS.target_tmp)
   except KeyError:
     print "no bootloader.img in target target_files; skipping install"
 
