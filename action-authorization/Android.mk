8046ad17dc061b884439d8b0c98a893f67e07a4f
tanminger <ming.tan@intel.com>
2017-06-05 16:33:46 +0800

    Add the tool action-authorization.
    Copied from Cherry Trail source code.
    
    If want to set the lock state of a user build device form locked
    to unlocked, then need this tool to generate the PKCS7 message
    for action-authorization.
    Only Linux host tool is supported.
    
    JIRA: None.
    Test: Test it on Joule.
    
    Signed-off-by: tanminger <ming.tan@intel.com>

---
+++ action-authorization/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_LDLIBS_windows := -lws2_32 -lgdi32
+LOCAL_LDLIBS_linux := -lrt -ldl -lpthread
+LOCAL_CXX_STL := libc++_static
+
+LOCAL_MODULE := action-authorization
+#LOCAL_MODULE_HOST_OS := linux windows
+LOCAL_SRC_FILES := action-authorization.c
+LOCAL_C_INCLUDES := vendor/intel/external/openssl/include/
+LOCAL_CFLAGS := -Wall -Wextra -Werror
+LOCAL_STATIC_LIBRARIES := libcrypto_static2
+
+include $(BUILD_HOST_EXECUTABLE)
