609c52202a7fb7e5a1f5ccbec953f509fa30d2d1
Mingwei Shi <mingwei.shi@intel.com>
2017-07-04 19:38:31 +0800

    [VNDK] Move ioc_slcand to vendor partition
    Google Treble project require to separate Android Framework and Vendor OTA
    Framework might upgrade to new version without vendor implementation change
    In oder to achieve the target, require installing all of vendor modules to
    vendor partition.
    
    If don't moving the module to vendor partition, when Google upgrade the OS,
    we will missing all modules located in root partition and system partition.
    
    Change-Id: I62412767297cf3796afaeb737512a231de886d8a
    Tracked-On: OAM-47394
    Signed-off-by: Mingwei Shi <mingwei.shi@intel.com>
    Reviewed-on: 591181

--- ioc_slcand/Android.mk
+++ ioc_slcand/Android.mk
@@ -2,10 +2,10 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := ioc_slcand
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_LIBRARIES += liblog libcutils libc
 LOCAL_SRC_FILES := ioc_slcand.c
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/sbin
 
 include $(BUILD_EXECUTABLE)
