8e525588b1265c782873fed107649ad918d35ef6
Zhang Ning <ning.a.zhang@intel.com>
2017-12-04 10:40:15 +0800

    filesystem config for load_modules
    remove load_audio_modules filesystem config
    add load_modules filesystem config
    
    Change-Id: I1d7cd262ce884c6de28f66c1a7bab98ed4d6af03
    Tracked-On: OAM-53531
    Signed-off-by: Zhang Ning <ning.a.zhang@intel.com>
    Reviewed-on: 610503

--- audio/filesystem_config/config.fs
+++
@@ -1,9 +0,0 @@
-[AID_AUDIOHAL]
-value:5200
-
-[vendor/bin/load_audio_modules.sh]
-mode: 0755
-user: AID_AUDIOHAL
-group: shell
-caps: 0
-
---
+++ load_modules/filesystem_config/config.fs
@@ -0,0 +1,8 @@
+[AID_LOAD_MODULE]
+value:5200
+
+[vendor/bin/load_modules.sh]
+mode: 0755
+user: AID_LOAD_MODULE
+group: shell
+caps: 0
