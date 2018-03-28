ff86b449fccf48330120c369152e576ff62991cb
li zhuangzhi <zhuangzhi.li@intel.com>
2018-03-28 14:45:37 +0800

    Clean up user space code in android common config
    Change-Id: Ic36cca27ce4c00ba868e227d750034af4dbb21d4
    Tracked-On: OAM-60838
    Signed-off-by: li zhuangzhi <zhuangzhi.li@intel.com>
    Reviewed-on: 624170

--- feature_config/Comments.java
+++
@@ -1,8 +0,0 @@
-/*
- * This file only contains comments. It is required by the build system
- * that does not accept to build a java library without any files specified in
- * LOCAL_SRC_FILES.
- *
- * The important source file for this library is generated based on a template
- * and enabled feature flags.
- */
