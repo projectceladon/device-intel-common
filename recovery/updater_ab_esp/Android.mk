4fbb0c3d5946bec10a32ac4c9a3b948ead713530
Ming Tan <ming.tan@intel.com>
2018-04-24 10:14:52 +0800

    Add the updater_ab_esp, used for update kernelflinger.efi.
    It will be called in postinstall stage in OTA update with A/B slot.
    Currently the code just print the tool info.
    
    Jira: None.
    Test: Test it in KBL NUC.
    
    Signed-off-by: Ming Tan <ming.tan@intel.com>
    Signed-off-by: Zhou, Lihua <lihuax.zhou@intel.com>

---
+++ recovery/updater_ab_esp/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE:= updater_ab_esp
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := updater_ab_esp.cpp
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+include $(BUILD_EXECUTABLE)
