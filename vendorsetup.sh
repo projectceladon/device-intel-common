41c9814b05277ab35663d1f36ecec79f5830a110
Chen Lin Z <lin.z.chen@intel.com>
2018-08-24 17:07:08 +0800

    Remove mixins check rule
    mixins check will be executed in lunch step
    
    Change-Id: I1dbde9dca49a25492dc7d70c6b8fb5ef715f9498
    Tracked-On: OAM-67983
    Signed-off-by: Chen Lin Z <lin.z.chen@intel.com>
    Reviewed-on: 642811

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,3 @@
-if [ -f device/intel/mixins/mixin-update ]; then
-    if ! device/intel/mixins/mixin-update --dry-run; then
-        echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
-        echo '+ Product configuration and mixins are out of sync                      +'
-        echo '+ PLEASE RE-RUN device/intel/mixins/mixin-update and commit the result! +'
-        echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
-    fi
-fi
-
 mkernel() {
     local T=$(gettop)
     local KERNEL_MAKEFILE=$(\cd "$T" && find device -name ${TARGET_PRODUCT}.mk | sed -e "s/${TARGET_PRODUCT}\.mk/AndroidBoard\.mk/")
