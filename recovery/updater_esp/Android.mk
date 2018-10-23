87d8367a43cf559fe98de52b2f32b8cea9a3dc5a
Meng, KangX <kangx.meng@intel.com>
2018-10-23 10:59:53 +0800

    Fix the compile error when disable AVB and slot-ab.
    Tracked-On: OAM-69456
    
    Signed-off-by: Meng, KangX <kangx.meng@intel.com>

--- recovery/updater_esp/Android.mk
+++ recovery/updater_esp/Android.mk
@@ -4,10 +4,12 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := updater_esp.cpp
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := bootable/recovery \
-		    bootable/userfastboot/libgpt/include \
+		    $(LOCAL_PATH)/../libgpt/include \
 		    $(LOCAL_PATH)/../common_recovery
 LOCAL_MODULE := libupdater_esp
 LOCAL_CFLAGS := -Wall  -DUEFI_ARCH=\"$(TARGET_UEFI_ARCH)\"
+
+LOCAL_STATIC_LIBRARIES := libotautil libedify
 
 ifeq ($(TARGET_SUPPORT_BOOT_OPTION),true)
     LOCAL_STATIC_LIBRARIES := libefivar
