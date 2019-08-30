d3b33d21e4a881278b101832700a37f65e2c4320
Chen Lin Z <lin.z.chen@intel.com>
2019-08-30 14:10:24 +0800

    Include all firmwares to support more hardware platform
    Change-Id: I35873ed4f4d5a6422d923aa6698f15e50aaf7ce2
    Tracked-On:
    Signed-off-by: Chen Lin Z <lin.z.chen@intel.com>

--- firmware.mk
+++ firmware.mk
@@ -14,33 +14,10 @@
 # limitations under the License.
 #
 
-## List of Firmware bin files to be copied
+FIND_IGNORE_FILES := -not -name "*\ *" -not -name "*LICENSE.*" -not -name "*LICENCE.*" \
+	-not -name "*Makefile*" -not -name "*WHENCE" -not -name "*README"
 
-LOCAL_FIRMWARE_SRC := \
-    i6050-fw-usb-1.5.sbcf \
-    i2400m-fw-usb-1.4.sbcf \
-    i2400m-fw-usb-1.5.sbcf \
-    iwlwifi-3168-29.ucode \
-    iwlwifi-8265-31.ucode \
-    iwlwifi-9000-pu-b0-jf-b0-38.ucode \
-    iwlwifi-9000-pu-b0-jf-b0-41.ucode \
-    iwlwifi-9260-th-b0-jf-b0-38.ucode \
-    iwlwifi-9260-th-b0-jf-b0-41.ucode \
-    iwlwifi-9260-th-b0-jf-b0-43.ucode
-
-## List of complete Firmware folders to be copied
-
-LOCAL_FIRMWARE_DIR := \
-    intel \
-    i915
-
-## List of matching patterns of Firmware bins to be copied
-
-#LOCAL_FIRMWARE_PATTERN := \
-#    iwlwifi
-
-LOCAL_FIRMWARE_SRC += $(foreach f,$(LOCAL_FIRMWARE_PATTERN),$(shell cd $(FIRMWARES_DIR) && find . -iname "*$(f)*" -type f ))
-LOCAL_FIRMWARE_SRC += $(foreach f,$(LOCAL_FIRMWARE_DIR),$(shell cd $(FIRMWARES_DIR) && find $(f) -type f) )
+LOCAL_FIRMWARES ?= $(filter-out .git/% %.mk,$(subst ./,,$(shell cd $(FIRMWARES_DIR) && find . -type f $(FIND_IGNORE_FILES))))
 
 PRODUCT_COPY_FILES := \
-    $(foreach f,$(LOCAL_FIRMWARE_SRC),$(FIRMWARES_DIR)/$(f):$(TARGET_COPY_OUT_VENDOR)/firmware/$(f))
+	    $(foreach f,$(LOCAL_FIRMWARES),$(FIRMWARES_DIR)/$(f):vendor/firmware/$(f))
