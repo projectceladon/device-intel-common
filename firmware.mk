25928173b303ed5b851bb2af7c15a889dfc4d149
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2020-11-02 17:37:32 +0530

    Fix issue in copying firmware files based on folder and pattern
    Copying of firmware files based on folder and pattern failed
    due to symbolic link files not getting copied.
    
    Fix the issue by adding l to find commands type option. Remove
    the copy of individual files as it is either not needed or
    covered as part of firmware pattern.
    
    Tracked-On: OAM-94529
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- firmware.mk
+++ firmware.mk
@@ -26,33 +26,17 @@ PRODUCT_COPY_FILES := \
 
 else
 
-## List of Firmware bin files to be copied
-
-LOCAL_FIRMWARE_SRC := \
-    i6050-fw-usb-1.5.sbcf \
-    i2400m-fw-usb-1.4.sbcf \
-    i2400m-fw-usb-1.5.sbcf \
-    iwlwifi-3168-29.ucode \
-    iwlwifi-8265-31.ucode \
-    iwlwifi-9000-pu-b0-jf-b0-38.ucode \
-    iwlwifi-9000-pu-b0-jf-b0-41.ucode \
-    iwlwifi-9260-th-b0-jf-b0-38.ucode \
-    iwlwifi-9260-th-b0-jf-b0-41.ucode \
-    iwlwifi-9260-th-b0-jf-b0-43.ucode
-
 ## List of complete Firmware folders to be copied
-
 LOCAL_FIRMWARE_DIR := \
     intel \
     i915
 
 ## List of matching patterns of Firmware bins to be copied
+LOCAL_FIRMWARE_PATTERN := \
+    iwlwifi
 
-#LOCAL_FIRMWARE_PATTERN := \
-#    iwlwifi
-
-LOCAL_FIRMWARE_SRC += $(foreach f,$(LOCAL_FIRMWARE_PATTERN),$(shell cd $(FIRMWARES_DIR) && find . -iname "*$(f)*" -type f ))
-LOCAL_FIRMWARE_SRC += $(foreach f,$(LOCAL_FIRMWARE_DIR),$(shell cd $(FIRMWARES_DIR) && find $(f) -type f) )
+LOCAL_FIRMWARE_SRC += $(foreach f,$(LOCAL_FIRMWARE_PATTERN),$(shell cd $(FIRMWARES_DIR) && find . -iname "*$(f)*" -type f,l ))
+LOCAL_FIRMWARE_SRC += $(foreach f,$(LOCAL_FIRMWARE_DIR),$(shell cd $(FIRMWARES_DIR) && find $(f) -type f,l) )
 
 PRODUCT_COPY_FILES := \
     $(foreach f,$(LOCAL_FIRMWARE_SRC),$(FIRMWARES_DIR)/$(f):$(TARGET_COPY_OUT_VENDOR)/firmware/$(f))
