44baac7a661bafe3218e7696cdf5498cd108a904
Ratnesh Kumar Rai <ratnesh.kumar.rai@intel.com>
2023-04-07 00:39:48 +0530

    Enable GPU device for remote infer
    download and install intel compute runtime packages required
    for GPU inference
    
    to setup inference via GPU, run sudo -E scripts/setup_host.sh -i GPU
    
    Tracked-On: OAM-108702
    Signed-off-by: Ratnesh Kumar Rai <ratnesh.kumar.rai@intel.com>

--- host_scripts/setup_ai_dispatcher.sh
+++ host_scripts/setup_ai_dispatcher.sh
@@ -16,21 +16,22 @@ GRPC_PATH=$CIV_WORK_DIR/grpc
 
 #---------      Functions      -------------------
 function install_pre_requisites() {
-    sudo apt update
-    sudo apt-get install -y git python3 python-dev 
+    apt update
+    apt-get install -y git python3 python-dev
+    export PYTHONPATH="$HOME/.local/bin:${PYTHONPATH}"
 
     if [ ! -x "$(command -v pip3)" ]; then
         echo " Installing Python-pip"
-        sudo apt-get -y install python3-pip
+        apt-get -y install python3-pip
     fi
 
     version=$(pip3 -V 2>&1 | grep -Po '(?<=pip )\d+')
-    if [ "$version" -lt "19" ]; then
+    if [ "$version" -lt "23" ]; then
         echo "Upgrading Python-pip version"
         pip3 install --upgrade pip
     fi
 
-    python3 -m pip install six openvino-dev==2022.3.0
+    python3 -m pip install --ignore-installed six openvino-dev==2022.3.0
     echo "Installing pre-requisites"
 }
 
@@ -51,20 +52,20 @@ function setup_ai_dispatcher() {
 
     echo "create model directory"
     if [ -d "$MODEL_DIR" ]; then
-        sudo rm -rfd $MODEL_DIR
+        rm -rfd $MODEL_DIR
     fi
-    sudo mkdir -p $MODEL_DIR
+    mkdir -p $MODEL_DIR
 
     echo "Checking for environment PYTHON PATH"
     a=`grep -rn "PYTHONPATH\=\".*ai-dispatcher.*\"" /home/$SUDO_USER/.bashrc`
     b=`grep -rn "PYTHONPATH" /home/$SUDO_USER/.bashrc`
     if [ -z "$b" ]; then
-        echo "export PYTHONPATH="$PYTHONPATH:$AI_DISPATCHER_DIR"" | sudo tee -a /home/$SUDO_USER/.bashrc
+        echo "export PYTHONPATH="$PYTHONPATH:$HOME/.local/bin:$AI_DISPATCHER_DIR"" | sudo tee -a $HOME/.bashrc
     elif [ -z "$a" ]; then
-        sudo sed -i "s|PYTHONPATH.*||g" $HOME/.bashrc
-        echo "export  PYTHONPATH="$PYTHONPATH:$AI_DISPATCHER_DIR"" | sudo tee -a /home/$SUDO_USER/.bashrc
+        sed -i "s|PYTHONPATH.*||g" $HOME/.bashrc
+        echo "export PYTHONPATH="$PYTHONPATH:$HOME/.local/bin:$AI_DISPATCHER_DIR"" | sudo tee -a $HOME/.bashrc
     fi
-    sudo chown -R $SUDO_USER:$SUDO_USER $AI_DISPATCHER_DIR
+    chown -R $SUDO_USER:$SUDO_USER $AI_DISPATCHER_DIR
 }
 
 function setup_grpc() {
@@ -72,36 +73,54 @@ function setup_grpc() {
     if [ ! -e "$GRPC_PATH" ]; then
         git clone -b v1.46.2 https://github.com/grpc/grpc.git $GRPC_PATH && cd $GRPC_PATH
         git submodule update --init
-        git am $CIV_WORK_DIR/patches/grpc-host/0001-support-vsock-v1.46.2.patch
+        echo "applying vsock patch"
+        git apply -3<$CIV_WORK_DIR/patches/grpc-host/0001-support-vsock-v1.46.2.patch
         cd $GRPC_PATH/third_party/zlib
-        git am  $CIV_WORK_DIR/patches/grpc-host/0002-Fix-a-bug-when-getting-a-gzip-header-extra-field-wit.patch
+        echo "applying zlip patch"
+        git apply -3<$CIV_WORK_DIR/patches/grpc-host/0002-Fix-a-bug-when-getting-a-gzip-header-extra-field-wit.patch
         cd $GRPC_PATH
     else
         cd $GRPC_PATH
         echo "grpc repo already cloned, skipping cloning..."
     fi
-    pip install -r requirements.txt
-    GRPC_PYTHON_BUILD_WITH_CYTHON=1 pip install .
+    pip3 install -r requirements.txt
+    GRPC_PYTHON_BUILD_WITH_CYTHON=1 pip3 install .
+    cd $CIV_WORK_DIR
 }
 
 function check_env_proxy() {
-    a=`grep -rn "no_proxy\=\".*localhost.*\"" /home/$SUDO_USER/.bashrc`
-    b=`grep -rn "no_proxy" /home/$SUDO_USER/.bashrc`
+    a=`grep -rn "no_proxy\=\".*localhost.*\"" $HOME/.bashrc`
+    b=`grep -rn "no_proxy" $HOME/.bashrc`
     echo "Checking for environment no_proxy settings"
 
     if [ -z "$b" ]; then
-        echo "export no_proxy=\"localhost\"" | sudo tee -a /home/$SUDO_USER/.bashrc
+        echo "export no_proxy=\"localhost\"" | sudo tee -a $HOME/.bashrc
     elif [ -z "$a" ]; then
-        sudo sed -i "s|export no_proxy.*||g" $HOME/.bashrc
-        echo "export no_proxy=\"$no_proxy,localhost\"" | sudo tee -a /home/$SUDO_USER/.bashrc
+        sed -i "s|export no_proxy.*||g" $HOME/.bashrc
+        echo "export no_proxy=\"$no_proxy,localhost\"" | sudo tee -a $HOME/.bashrc
     fi
+}
+
+function setup_gpu_drivers() {
+    echo "setting up gpu drivers"
+    sudo mkdir -p $CIV_WORK_DIR/neo && cd $CIV_WORK_DIR/neo
+    wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.13230.7/intel-igc-core_1.0.13230.7_amd64.deb
+    wget https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.13230.7/intel-igc-opencl_1.0.13230.7_amd64.deb
+    wget https://github.com/intel/compute-runtime/releases/download/23.05.25593.11/intel-level-zero-gpu-dbgsym_1.3.25593.11_amd64.ddeb
+    wget https://github.com/intel/compute-runtime/releases/download/23.05.25593.11/intel-level-zero-gpu_1.3.25593.11_amd64.deb
+    wget https://github.com/intel/compute-runtime/releases/download/23.05.25593.11/intel-opencl-icd-dbgsym_23.05.25593.11_amd64.ddeb
+    wget https://github.com/intel/compute-runtime/releases/download/23.05.25593.11/intel-opencl-icd_23.05.25593.11_amd64.deb
+    wget https://github.com/intel/compute-runtime/releases/download/23.05.25593.11/libigdgmm12_22.3.0_amd64.deb
+    dpkg -i *.deb
+    cd $CIV_WORK_DIR
+    rm -rfd $CIV_WORK_DIR/neo
 }
 
 function setup_infer_service() {
     echo "Adding AI-Dispatcher rawTensor Service"
-    sudo touch /lib/systemd/system/ai_raw_tensor.service
+    touch /lib/systemd/system/ai_raw_tensor.service
 
-    cat << EOF | sudo tee /lib/systemd/system/ai_raw_tensor.service
+    cat << EOF | tee /lib/systemd/system/ai_raw_tensor.service
 [Unit]
 Description="Start/Stop Remote Inferencing"
 
@@ -109,13 +128,14 @@ Description="Start/Stop Remote Inferencing"
 User=$SUDO_USER
 WorkingDirectory=$(pwd)/
 Type=simple
-ExecStart=/bin/bash ../scripts/ai_raw_tensor.sh $1
+ExecStart=/bin/bash ./scripts/ai_raw_tensor.sh $1
 
 [Install]
 WantedBy=multi-user.target
 EOF
-    sudo systemctl enable ai_raw_tensor
-    sudo systemctl start ai_raw_tensor
+    systemctl enable ai_raw_tensor
+    systemctl daemon-reload
+    systemctl start ai_raw_tensor
     echo "Starting Remote Inferencing Setup Script"
 }
 
@@ -123,8 +143,11 @@ EOF
 
 check_env_proxy
 install_pre_requisites
-setup_grpc
 setup_ai_dispatcher
+setup_grpc
+if [ $1 =~ "GPU" -o $1 == "AUTO" ]; then
+    setup_gpu_drivers
+fi
 setup_infer_service $1
 
 echo "remote infer installation done"
